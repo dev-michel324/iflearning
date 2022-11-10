513df313b4a6d1dce864e5d492e08ab866ef3eae
dev-michel324 <dev_michel324@protonmail.com>
2022-11-10 15:17:50 -0300

    fix: removed unnecessary fields

--- accounts/models.py
+++ accounts/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-SCHOOL_GRADE = (
-    ("Ensino Fundamental", "Ensino Fundamental"),
-    ("Ensino Médio", "Ensino Médio"),
-    ("Ensino Superior", "Ensino Superior")
-)
+# SCHOOL_GRADE = (
+#     ("Ensino Fundamental", "Ensino Fundamental"),
+#     ("Ensino Médio", "Ensino Médio"),
+#     ("Ensino Superior", "Ensino Superior")
+# )
 
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True, blank=True)
-    birth = models.DateField(null=True)
-    school_grade = models.CharField(
-        choices=SCHOOL_GRADE, max_length=19, default="Ensino Médio")
+    # birth = models.DateField(null=True)
+    # school_grade = models.CharField(
+    #     choices=SCHOOL_GRADE, max_length=19, default="Ensino Médio")
 
     def __str__(self):
         return self.username
